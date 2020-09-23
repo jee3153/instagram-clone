7b40c11572ef3e7f8edfcda78d84a63c6797e099
Jenny Yang <michel3153@gmail.com>
2020-09-23 20:57:36 +0100

    Trying again to deploy

--- config/urls.py
+++ config/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += [path("__debug__/", include(debug_toolbar.urls)),] + static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

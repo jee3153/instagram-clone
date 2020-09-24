57917dc2c57724d8b895868221abea4f26967754
Jenny Yang <michel3153@gmail.com>
2020-09-24 10:00:09 +0100

    Added staticfiles directory

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 AUTH_USER_MODEL = "accounts.User"

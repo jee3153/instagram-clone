ac77125abe5072f4c6dfcee79191d7842dba71a9
Jenny Yang <michel3153@gmail.com>
2020-09-24 10:54:56 +0100

    added region AWS

--- config/settings.py
+++ config/settings.py
@@ -162,6 +162,10 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
+AWS_S3_HOST = os.environ.get("AWS_S3_HOST")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 django_heroku.settings(locals())
